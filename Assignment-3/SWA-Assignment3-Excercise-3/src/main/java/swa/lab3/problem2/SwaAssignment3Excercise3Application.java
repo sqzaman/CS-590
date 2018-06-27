500e6a37614a4bf1714fe3285df0b165f669e11a
sqzaman <sqzaman@gmail.com>
2018-06-27 17:59:45 -0500

    initial commit

---
+++ Assignment-3/SWA-Assignment3-Excercise-3/src/main/java/swa/lab3/problem2/SwaAssignment3Excercise3Application.java
@@ -0,0 +1,12 @@
+package swa.lab3.problem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaAssignment3Excercise3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaAssignment3Excercise3Application.class, args);
+	}
+}
