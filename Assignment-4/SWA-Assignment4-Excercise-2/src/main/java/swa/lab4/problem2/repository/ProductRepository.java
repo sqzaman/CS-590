0530a919c142b61593c84d3a2aff65f06ce0008c
sqzaman <sqzaman@gmail.com>
2018-07-02 20:31:28 -0500

    updated

--- Assignment-4/SWA-Assignment4-Excercise-2/src/main/java/swa/lab4/problem2/repository/ProductRepository.java
+++ Assignment-4/SWA-Assignment4-Excercise-2/src/main/java/swa/lab4/problem2/repository/ProductRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import swa.lab4.problem2.domain.entity.Product;
 
 
-
-
 public interface ProductRepository extends MongoRepository<Product, String>  {
 
 }
