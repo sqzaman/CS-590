34beaf56239a48537414fd71ecd9a15e336c7443
sqzaman <sqzaman@gmail.com>
2018-07-03 14:45:03 -0500

    updated

--- Assignment-7/OrderServiceIntregration-2/src/main/java/integration/NormalShippingService.java
+++ Assignment-7/OrderServiceIntregration-2/src/main/java/integration/NormalShippingService.java
@@ -2,7 +2,7 @@ package integration;
 
 public class NormalShippingService {
 	public void handle() throws Exception {
-		System.out.println("Shipping service got a new order total amount is greater than 175");
+		System.out.println("Shipping service got a new order total amount is less than 175");
 	}
 
 }
